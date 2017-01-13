2563f94dbae134d035b6d73f200f6893a281d07b
PRob <PRob>
2017-01-13 10:07:51 -0500

    homework v.1

---
+++ ex3.rb
@@ -0,0 +1,22 @@
+puts "I will now count my chickens:"
+
+puts "Hens #{25.0 + 30/6}" #text and then # to show result of equation after text
+puts "Roosters #{100 - 25 * 3.0 % 4.0}"
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4.0 % 2.0 - 1 / 4 + 6 #don't need to use # because it is not embedded with text
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7 #ruby does the math and can read a logic statement for true/false
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}" #true or false output after text
+puts "Is it less or equal? #{5 <= -2}"
---
+++ ex3b.rb
@@ -0,0 +1,3 @@
+puts "How many beers do you drink a week? #{7 * 6}"
+
+puts "That's more than on a week with a Steelers Game! #{42 > 70}"
